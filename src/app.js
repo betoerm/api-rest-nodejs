64c8432867d1e50d72d428825d94cb5e2764e943
beto <betoer@gmail.com>
2019-06-14 22:36:51 -0300

    inserção da rota produtos

--- src/app.js
+++ src/app.js
@@ -6,11 +6,15 @@ const router = express.Router();
 //Rotas
 const index = require('./routes/index');
 const personRoute = require('./routes/personRoute');
+const productsRoute = require('./routes/productsRoute');
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 app.use('/', index);
 app.use('/person', personRoute);
+app.use('/products', productsRoute);
+
+
 
 module.exports = app;
